347304af85371400d10bfee93e7e6990e74e50d8
Sebastien <sebastien@sebs-studio.com>
2013-06-07 20:19:40 +0200

    GitHub release and version 2.3

---
+++ readme.txt
@@ -0,0 +1,113 @@
+=== WooCommerce Video Product Tab ===
+Contributors: sebd86
+Tags: ecommerce, e-commerce, commerce, sebs studio, woocommerce, video, extension, product, embed, tab
+Donate link: http://www.sebs-studio.com/donation/
+Requires at least: 3.3
+Tested up to: 3.5.1
+Stable tag: trunk
+License: GPLv3
+License URI: http://www.gnu.org/licenses/gpl-3.0.html
+
+Extends WooCommerce to allow you to add a Video to the Product page. Creates a new tab to display your embeded video.
+
+== Description ==
+
+Extends WooCommerce to allow you to add a Video to the Product page. An additional tab is added on the single products page to allow your customers to view the video you embeded.
+
+When adding a product select the "Video" tab and insert your video embed code where asked. A new tab will appear on the product page called "Video" (or the title you entered for the video) and your video will then display.
+
+WooCommerce Video Product Tab works with any theme that supports WooCommerce.
+
+= Localization =
+English (default) - always included. *.po file (default.po) in language folder for translations.
+If you do a translation for your site please send it to me to include as part of the plugin language folder. I'll acknowledge your work here. To send your translation files contact me. (http://www.sebs-studio.com/contact/)
+
+= Related Plugins =
+
+WooCommerce Photos Product Tab - http://wordpress.org/extend/plugins/woocommerce-photos-product-tab/
+WooCommerce YouTube Video Product Tab - http://www.sebs-studio.com/products/woocommerce-youtube-video-product-tab/
+
+= Documentation =
+
+For all documentation on this plugin go to: http://docs.sebs-studio.com/user-guide/extension/woocommerce-video-product-tab/
+
+== Installation ==
+
+= Minimum Requirements =
+
+* WooCommerce 1.6.2 or higher.
+
+= Automatic installation =
+
+Automatic installation is the easiest option as WordPress handles the file transfers itself and you don't even need to leave your web browser. To do an automatic install of WooCommerce Video Product Tab, log in to your WordPress admin panel, navigate to the Plugins menu and click Add New.
+
+In the search field type "WooCommerce Video Product Tab" and click Search Plugins. Once you've found my plugin extension you can view details about it such as the the point release, rating and description. Most importantly of course, you can install it by simply clicking Install Now. After clicking that link you will be asked if you're sure you want to install the plugin. Click yes and WordPress will automatically complete the installation.
+
+= Manual installation =
+
+The manual installation method involves downloading my plugin and uploading it to your webserver via your favourite FTP application.
+
+1. Download the plugin file to your computer and unzip it
+2. Using an FTP program, or your hosting control panel, upload the unzipped plugin folder to your WordPress installation's wp-content/plugins/ directory.
+3. Activate the plugin from the Plugins menu within the WordPress admin.
+
+= Upgrading =
+
+Automatic updates should work like a charm; as always though, ensure you backup your site just in case.
+
+== Frequently Asked Questions ==
+
+If you get stuck you can ask for help on the Support forum for the plugin  (http://wordpress.org/support/plugin/woocommerce-video-product-tab).
+
+= Will WooCommerce Video Product Tab work with my theme? =
+
+Yes; My plugin uses the same styling for any theme that is WooCommerce compaitable.
+
+= Where can I report bugs or contribute to the plugin? =
+
+Bugs can be reported in the support forum including your ideas or code snippets for the plugin. (http://wordpress.org/support/plugin/woocommerce-video-product-tab).
+
+= What videos can I embed ? =
+
+You can embed any video you want. If the video host allows you to embed on your site then just simply copy the embed code you want to add. No matter were it is hosted on, the video will display in the tab.
+
+= Can I embed more than one ? =
+
+Yes you can. Simply make a new line and paste the next embed code underneath or above the previously entered embed code.
+
+== Screenshots ==
+
+1. Product Page with a Video added.
+2. How to add a video for your product.
+
+== Changelog ==
+
+= 2.3 - 07/06/2013 = 
+* Added - Option to hide the title in the product tab.
+* Tweaked - The video title now displays as the tab name for WooCommerce 1.6.6 and below users.
+* Added - Link to WooCommerce YouTube Video Product Tab plugin in description.
+* Added - Link to documentation for the plugin in description.
+
+= 2.2 - 08/04/2013 = 
+* Added - Brazilian Portuguese translation by Johnny Bauer
+
+= 2.1 - 08/03/2013 = 
+* Fixed - Incorrect text domain on line 39.
+
+= 2.0 - 07/03/2013 = 
+* Tweak - Updated readme.txt file, improved changelog and updated GPL license version.
+* Fix - The plugin has been updated to work with WooCommerce version 2 and below.
+* Tweak - Added a play icon to match the other tab styles in the admin panel on the product edit page.
+* Tweak - Video tab works for the new admin layout and the old.
+* Feature - Now you can enter a title for the video rather than just having it say 'Video'.
+* Feature - Plugin is now localized.
+
+= 1.2 = 
+* Tweak - Video tab now uses 'wp_embed' for filtering the embed code.
+* Fix - Removed phonehome api.
+
+= 1.1 =
+* Fix - Compatibility with WooCommerce Custom Tab Lite plugin.
+
+= 1.0 =
+* Initial Release.
---
+++ woocommerce-video-tab.php
@@ -0,0 +1,349 @@
+<?php
+/*
+ * Plugin Name: WooCommerce Video Product Tab
+ * Plugin URI: http://www.sebs-studio.com/wp-plugins/woocommerce-video-product-tab
+ * Description: Extends WooCommerce to allow you to add a Video to the Product page. An additional tab is added on the single products page to allow your customers to view the video you embeded. 
+ * Version: 2.3
+ * Author: Sebs Studio (Sebastien)
+ * Author URI: http://www.sebs-studio.com
+ *
+ * Text Domain: wc_video_product_tab
+ * Domain Path: /lang/
+ *
+ */
+
+if(!defined('ABSPATH')) exit; // Exit if accessed directly
+
+// Required minimum version of WordPress.
+if(!function_exists('woo_video_tab_min_required')){
+	function woo_video_tab_min_required(){
+		global $wp_version;
+		$plugin = plugin_basename(__FILE__);
+		$plugin_data = get_plugin_data(__FILE__, false);
+
+		if(version_compare($wp_version, "3.3", "<")){
+			if(is_plugin_active($plugin)){
+				deactivate_plugins($plugin);
+				wp_die("'".$plugin_data['Name']."' requires WordPress 3.3 or higher, and has been deactivated! Please upgrade WordPress and try again.<br /><br />Back to <a href='".admin_url()."'>WordPress Admin</a>.");
+			}
+		}
+	}
+	add_action('admin_init', 'woo_video_tab_min_required');
+}
+
+// Checks if the WooCommerce plugins is installed and active.
+if(in_array('woocommerce/woocommerce.php', apply_filters('active_plugins', get_option('active_plugins')))){
+
+	/* Localisation */
+	$locale = apply_filters('plugin_locale', get_locale(), 'woocommerce-video-product-tab');
+	load_textdomain('wc_video_product_tab', WP_PLUGIN_DIR."/".plugin_basename(dirname(__FILE__)).'/lang/wc_video_product_tab-'.$locale.'.mo');
+	load_plugin_textdomain('wc_video_product_tab', false, dirname(plugin_basename(__FILE__)).'/lang/');
+
+	if(!class_exists('WooCommerce_Video_Product_Tab')){
+		class WooCommerce_Video_Product_Tab{
+
+			public static $plugin_prefix;
+			public static $plugin_url;
+			public static $plugin_path;
+			public static $plugin_basefile;
+
+			private $tab_data = false;
+
+			/**
+			 * Gets things started by adding an action to initialize this plugin once
+			 * WooCommerce is known to be active and initialized
+			 */
+			public function __construct(){
+				WooCommerce_Video_Product_Tab::$plugin_prefix = 'wc_video_tab_';
+				WooCommerce_Video_Product_Tab::$plugin_basefile = plugin_basename(__FILE__);
+				WooCommerce_Video_Product_Tab::$plugin_url = plugin_dir_url(WooCommerce_Video_Product_Tab::$plugin_basefile);
+				WooCommerce_Video_Product_Tab::$plugin_path = trailingslashit(dirname(__FILE__));
+				add_action('woocommerce_init', array(&$this, 'init'));
+			}
+
+			/**
+			 * Init WooCommerce Video Product Tab extension once we know WooCommerce is active
+			 */
+			public function init(){
+				// backend stuff
+				add_filter('plugin_row_meta', array($this, 'add_support_link'), 10, 2);
+				add_action('woocommerce_product_write_panel_tabs', array($this, 'product_write_panel_tab'));
+				add_action('woocommerce_product_write_panels', array($this, 'product_write_panel'));
+				add_action('woocommerce_process_product_meta', array($this, 'product_save_data'), 10, 2);
+				// frontend stuff
+				if(version_compare(WOOCOMMERCE_VERSION, "2.0", '>=')){
+					// WC >= 2.0
+					add_filter('woocommerce_product_tabs', array($this, 'video_product_tabs_two'));
+				}
+				else{
+					add_action('woocommerce_product_tabs', array($this, 'video_product_tabs'), 25);
+					// in between the attributes and reviews panels.
+					add_action('woocommerce_product_tab_panels', array($this, 'video_product_tabs_panel'), 25);
+				}
+			}
+
+			/**
+			 * Add donation link to plugin page.
+			 */
+			public function add_support_link($links, $file){
+				if(!current_user_can('install_plugins')){
+					return $links;
+				}
+				if($file == WooCommerce_Video_Product_Tab::$plugin_basefile){
+					$links[] = '<a href="http://www.sebs-studio.com/donation/" target="_blank">'.__('Donate', 'wc_video_product_tab').'</a>';
+					$links[] = '<a href="http://www.sebs-studio.com/wp-plugins/woocommerce-extensions/" target="_blank">'.__('More WooCommerce Extensions', 'wc_video_product_tab').'</a>';
+				}
+				return $links;
+			}
+
+			/**
+			 * Write the video tab on the product view page for WC 2.0+.
+			 * In WooCommerce these are handled by templates.
+			 */
+			public function video_product_tabs_two($tabs){
+				global $product;
+
+				if($this->product_has_video_tabs($product)){
+					foreach($this->tab_data as $tab){
+						$tabs[$tab['id']] = array(
+												'title'    => $tab['title'],
+												'priority' => 25,
+												'callback' => array($this, 'video_product_tabs_panel_content'),
+												'content'  => $tab['video']
+						);
+					}
+				}
+				return $tabs;
+			}
+
+			/**
+			 * Write the video tab on the product view page for WC 1.6.6 and below.
+			 * In WooCommerce these are handled by templates.
+			 */
+			public function video_product_tabs(){
+				global $product;
+
+				if($this->product_has_video_tabs($product)){
+					foreach($this->tab_data as $tab){
+						echo "<li><a href=\"#{$tab['id']}\">".$tab['title']."</a></li>";
+					}
+				}
+			}
+
+			/**
+			 * Write the video tab panel on the product view page WC 2.0+.
+			 * In WooCommerce these are handled by templates.
+			 */
+			public function video_product_tabs_panel_content(){
+				global $product;
+
+				$embed = new WP_Embed(); // Since version 1.2
+
+				if($this->product_has_video_tabs($product)){
+					foreach($this->tab_data as $tab){
+						if($tab['hide_title'] == '') echo '<h2>'.$tab['title'].'</h2>';
+						echo $embed->autoembed(apply_filters('woocommerce_video_product_tab', $tab['video'], $tab['id']));
+					}
+				}
+			}
+
+			/**
+			 * Write the video tab panel on the product view page for WC 1.6.6 and below.
+			 * In WooCommerce these are handled by templates.
+			 */
+			public function video_product_tabs_panel(){
+				global $product;
+
+				$embed = new WP_Embed(); // Since version 1.2
+
+				if($this->product_has_video_tabs($product)){
+					foreach($this->tab_data as $tab){
+						echo '<div class="panel" id="'.$tab['id'].'">';
+						if($tab['hide_title'] == '') echo '<h2>'.$tab['title'].'</h2>';
+						echo $embed->autoembed(apply_filters('woocommerce_video_product_tab', $tab['video'], $tab['id'])); // Altered in version 1.2
+						echo '</div>';
+					}
+				}
+			}
+
+			/**
+			 * Lazy-load the product_tabs meta data, and return true if it exists,
+			 * false otherwise.
+			 * 
+			 * @return true if there is video tab data, false otherwise.
+			 */
+			private function product_has_video_tabs($product){
+				if($this->tab_data === false){
+					$this->tab_data = maybe_unserialize(get_post_meta($product->id, 'woo_video_product_tab', true));
+				}
+				// tab must at least have a embed code inserted.
+				return !empty($this->tab_data) && !empty($this->tab_data[0]) && !empty($this->tab_data[0]['video']);
+			}
+
+			/**
+			 * Adds a new tab to the Product Data postbox in the admin product interface
+			 */
+			public function product_write_panel_tab(){
+				$tab_icon = WooCommerce_Video_Product_Tab::$plugin_url.'play.png';
+
+				if(version_compare(WOOCOMMERCE_VERSION, "2.0.0") >= 0 ){
+					$style = 'padding:5px 5px 5px 28px; background-image:url('.$tab_icon.'); background-repeat:no-repeat; background-position:5px 7px;';
+					$active_style = '';
+				}
+				else{
+					$style = 'padding:9px 9px 9px 34px; line-height:16px; border-bottom:1px solid #d5d5d5; text-shadow:0 1px 1px #fff; color:#555555; background-image:url('.$tab_icon.'); background-repeat:no-repeat; background-position:9px 9px;';
+					$active_style = '#woocommerce-product-data ul.product_data_tabs li.my_plugin_tab.active a { border-bottom: 1px solid #F8F8F8; }';
+				}
+				?>
+				<style type="text/css">
+				#woocommerce-product-data ul.product_data_tabs li.video_tab a { <?php echo $style; ?> }
+				<?php echo $active_style; ?>
+				</style>
+				<?php
+				echo "<li class=\"video_tab\"><a href=\"#video_tab\">".__('Video', 'wc_video_product_tab')."</a></li>";
+			}
+
+			/**
+			 * Adds the panel to the Product Data postbox in the product interface
+			 */
+			public function product_write_panel(){
+				global $post;
+
+				// Pull the video tab data out of the database
+				$tab_data = maybe_unserialize(get_post_meta($post->ID, 'woo_video_product_tab', true));
+
+				if(empty($tab_data)){
+					$tab_data[] = array('title' => '', 'hide_title' => '', 'video' => '');
+				}
+
+				// Display the video tab panel
+				foreach($tab_data as $tab){
+					echo '<div id="video_tab" class="panel woocommerce_options_panel">';
+					$this->wc_video_product_tab_text_input(
+															array(
+																'id' => '_tab_video_title', 
+																'label' => __('Video Title', 'wc_video_product_tab'), 
+																'placeholder' => __('Enter your title here.', 'wc_video_product_tab'), 
+																'value' => $tab['title'], 
+																'style' => 'width:70%;',
+															)
+					);
+					woocommerce_wp_checkbox( array(
+												'id' => '_hide_title', 
+												'label' => __('Hide Title ?', 'wc_video_product_tab'), 
+												'description' => __('Enable this option to hide the title in the video tab.', 'wc_video_product_tab'), 
+												'value' => $tab['hide_title'], 
+											)
+					);
+					$this->wc_video_product_tab_textarea_input(
+															array(
+																'id' => '_tab_video', 
+																'label' => __('Embed Code', 'wc_video_product_tab'), 
+																'placeholder' => __('Place your video embed code here.', 'wc_video_product_tab'), 
+																'value' => $tab['video'], 
+																'style' => 'width:70%;height:140px;',
+															)
+					);
+					echo '</div>';
+				}
+			}
+
+			/**
+			 * Output a text input box.
+			 */
+			public function wc_video_product_tab_text_input($field){
+				global $thepostid, $post, $woocommerce;
+
+				$thepostid              = empty( $thepostid ) ? $post->ID : $thepostid;
+				$field['placeholder']   = isset( $field['placeholder'] ) ? $field['placeholder'] : '';
+				$field['class']         = isset( $field['class'] ) ? $field['class'] : 'short';
+				$field['wrapper_class'] = isset( $field['wrapper_class'] ) ? $field['wrapper_class'] : '';
+				$field['value']         = isset( $field['value'] ) ? $field['value'] : get_post_meta( $thepostid, $field['id'], true );
+				$field['name']          = isset( $field['name'] ) ? $field['name'] : $field['id'];
+				$field['type']          = isset( $field['type'] ) ? $field['type'] : 'text';
+
+				echo '<p class="form-field '.esc_attr($field['id']).'_field '.esc_attr($field['wrapper_class']).'"><label for="'.esc_attr($field['id']).'">'.wp_kses_post($field['label']).'</label><input type="'.esc_attr($field['type']).'" class="'.esc_attr($field['class']).'" name="'.esc_attr($field['name']).'" id="'.esc_attr($field['id']).'" value="'.esc_attr($field['value']).'" placeholder="'.esc_attr($field['placeholder']).'"'.(isset($field['style']) ? ' style="'.$field['style'].'"' : '').' /> ';
+
+				if(!empty($field['description'])){
+					if(isset($field['desc_tip'])){
+						echo '<img class="help_tip" data-tip="'.esc_attr($field['description']).'" src="'.$woocommerce->plugin_url().'/assets/images/help.png" height="16" width="16" />';
+					}
+					else{
+						echo '<span class="description">'.wp_kses_post($field['description']).'</span>';
+					}
+				}
+				echo '</p>';
+			}
+
+			/**
+			 * Output a textarea box.
+			 */
+			public function wc_video_product_tab_textarea_input($field){
+				global $thepostid, $post;
+
+				if(!$thepostid) $thepostid = $post->ID;
+				if(!isset($field['placeholder'])) $field['placeholder'] = '';
+				if(!isset($field['class'])) $field['class'] = 'short';
+				if(!isset($field['value'])) $field['value'] = get_post_meta($thepostid, $field['id'], true);
+
+				echo '<p class="form-field '.$field['id'].'_field"><label for="'.$field['id'].'">'.$field['label'].'</label><textarea class="'.$field['class'].'" name="'.$field['id'].'" id="'.$field['id'].'" placeholder="'.$field['placeholder'].'" rows="2" cols="20"'.(isset($field['style']) ? ' style="'.$field['style'].'"' : '').'">'.esc_textarea( $field['value']).'</textarea>';
+
+				if(isset($field['description']) && $field['description']) echo '<span class="description">' .$field['description'] . '</span>';
+
+				echo '</p>';
+			}
+
+			/**
+			 * Saves the data inputed into the product boxes, as post meta data
+			 * identified by the name 'woo_video_product_tab'
+			 * 
+			 * @param int $post_id the post (product) identifier
+			 * @param stdClass $post the post (product)
+			 */
+			public function product_save_data($post_id, $post){
+
+				$tab_title = stripslashes($_POST['_tab_video_title']);
+				if($tab_title == ''){
+					$tab_title = __('Video', 'wc_video_product_tab');
+				}
+				$hide_title = stripslashes($_POST['_hide_title']);
+				$tab_video = stripslashes($_POST['_tab_video']);
+
+				if(empty($tab_video) && get_post_meta($post_id, 'woo_video_product_tab', true)){
+					// clean up if the video tabs are removed
+					delete_post_meta($post_id, 'woo_video_product_tab');
+				}
+				elseif(!empty($tab_video)){
+					$tab_data = array();
+
+					$tab_id = '';
+					// convert the tab title into an id string
+					$tab_id = strtolower($tab_title);
+					$tab_id = preg_replace("/[^\w\s]/",'',$tab_id); // remove non-alphas, numbers, underscores or whitespace 
+					$tab_id = preg_replace("/_+/", ' ', $tab_id); // replace all underscores with single spaces
+					$tab_id = preg_replace("/\s+/", '-', $tab_id); // replace all multiple spaces with single dashes
+					$tab_id = 'tab-'.$tab_id; // prepend with 'tab-' string
+
+					// save the data to the database
+					$tab_data[] = array('title' => $tab_title, 'hide_title' => $hide_title, 'id' => $tab_id, 'video' => $tab_video);
+					update_post_meta($post_id, 'woo_video_product_tab', $tab_data);
+				}
+			}
+		}
+	}
+
+	/* 
+	 * Instantiate plugin class and add it to the set of globals.
+	 */
+	$woocommerce_video_tab = new WooCommerce_Video_Product_Tab();
+}
+else{
+	add_action('admin_notices', 'wc_video_tab_error_notice');
+	function wc_video_tab_error_notice(){
+		global $current_screen;
+		if($current_screen->parent_base == 'plugins'){
+			echo '<div class="error"><p>WooCommerce Video Product Tab '.__('requires <a href="http://www.woothemes.com/woocommerce/" target="_blank">WooCommerce</a> to be activated in order to work. Please install and activate <a href="'.admin_url('plugin-install.php?tab=search&type=term&s=WooCommerce').'" target="_blank">WooCommerce</a> first.', 'wc_video_product_tab').'</p></div>';
+		}
+	}
+}
+?>
